2f52050e5f77ca5617763c441720761abb1c272e
Stan Fan <fanhongy@amazon.com>
2024-03-07 22:09:59 +1100

    Initial commit

---
+++ test/eda-textrat-a2i.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EdaTextratA2I from '../lib/eda-textrat-a2i-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eda-textrat-a2i-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EdaTextratA2I.EdaTextratA2IStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
